b0733c6b0d2a56e9ebc91e2ca8a72281827e1dc3
Nguyen Khanh Duy <duynk@guava7.com>
2013-01-02 01:47:50 +0700

    - improve demo, switch mc animation

--- Demo/main.lua
+++ Demo/main.lua
@@ -9,6 +9,8 @@ end
 ResourceManagement.GetInstance():FreeCache()
 testAnim = {}
 
+
+--[[ TEST MEMORY LEAK ]]--
 print("Test memory leak")
 print(GetUsedMemory(true))
 for i=1, 100 do
@@ -24,18 +26,63 @@ print(GetUsedMemory(true))
 print(GetUsedMemory(true))
 
 
+--[[ TEST ANIMATION ]]--
 print("Test animation")
-local testAnim = CAnimation.CreateWithSpriteSheetName("Example", "Idle")
-local testAnim2 = CAnimation.CreateWithSpriteSheetName("Example", "Posture")
-testAnim:setPosition(200, 400)
-testAnim2:setPosition(500, 400)
-stage:addChild(testAnim)
-stage:addChild(testAnim2)
+
+local mc = CSprite.new()
+mc.spr = {
+	['Idle'] = CAnimation.CreateWithSpriteSheetName("Example", "Idle"),
+	['Posture'] = CAnimation.CreateWithSpriteSheetName("Example", "Posture"),
+}
+mc.sprStream = {
+	'Idle', 'Posture'
+} 
+mc.currentAnim = 1
+
+for _,v in pairs(mc.spr) do
+	v:SetVisible(false)
+	mc:addChild(v)
+end
+
+local screenW, screenH = application:getDeviceWidth(), application:getDeviceHeight()
+if application:getOrientation() == Stage.LANDSCAPE_LEFT or application:getOrientation() == Stage.LANDSCAPE_RIGHT then
+	if screenH > screenW then
+		screenW, screenH = screenH, screenW
+	end
+end
+
+mc:setScale(math.min(screenW/1024, screenH/768))
+mc:setPosition(screenW/2, 3*screenH/4)
+mc.spr[mc.sprStream[mc.currentAnim]]:setVisible(true)
+stage:addChild(mc)
+
+--label
+local label = TextField.new(nil, "Current Animation: " .. mc.sprStream[mc.currentAnim])
+label:setPosition(0, screenH - 2)
+label:setScale(3)
+stage:addChild(label)
+
+-- buttons
+local up = Bitmap.new(Texture.new("button/button_up.png"))
+local down = Bitmap.new(Texture.new("button/button_down.png"))
+local button = Button.new(up, down)
+button:addEventListener("click", 
+	function() 
+		mc.spr[mc.sprStream[mc.currentAnim]]:setVisible(false)
+		mc.currentAnim = mc.currentAnim + 1
+		if mc.currentAnim > #mc.sprStream then
+			mc.currentAnim = 1
+		end
+		mc.spr[mc.sprStream[mc.currentAnim]]:setVisible(true)
+		label:setText("Current Animation: " .. mc.sprStream[mc.currentAnim])
+	end)
+button:setPosition(0, 0)
+stage:addChild(button)
+
 
 
 function _onEnterFrame()
-	testAnim:Update(30)
-	testAnim2:Update(30)
+	mc.spr[mc.sprStream[mc.currentAnim]]:Update(30)
 end
 
 stage:addEventListener(Event.ENTER_FRAME, _onEnterFrame)
